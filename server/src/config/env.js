06daa330da649d5e2332ed67c075f81b8006efd4
1lelanta <kekikoki4@gmail.com>
2026-02-14 16:21:39 +0300

    create and add logic of environment variable

---
+++ server/src/config/env.js
@@ -0,0 +1,26 @@
+import dotenv from dotenv;
+
+dotenv.config();
+
+const requiredEnv = [
+    "PORT",
+    "MONGO_URI",
+    "JWT_SECRET",
+    "CLIENT_SECRET",
+    "CLIENT_URL"
+]
+
+requiredEnv.forEach((key)=>{
+    if(!process.env[key]){
+        throw new Error(`missing environment variable: ${key}`);
+
+    }
+});
+
+export const ENV = {
+    PORT: process.env.PORT,
+    MONGO_URI: process.env.MONGO_URI,
+    JWT_SECRET:process.env.JWT_SECRET,
+    CLIENT_URL:process.env.CLIENT_URL,
+
+}
