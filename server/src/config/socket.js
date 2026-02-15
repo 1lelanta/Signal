479ccf9b45403853d39b0483694cb4bf2c612172
1lelanta <kekikoki4@gmail.com>
2026-02-15 19:10:09 +0300

    add socket io to enable real-time chat

---
+++ server/src/config/socket.js
@@ -0,0 +1,30 @@
+import { Server} from "socket.io";
+import { ENV } from "./env.js";
+import { disconnect } from "mongoose";
+
+let io;
+
+export const initSocket = (server)=>{
+    io = new  Server(server, {
+        cors:{
+            origin:ENV.CLIENT_URL,
+            methods:["GET", "POST"],
+        }
+    });
+
+    io.on("connection", (socket)=>{
+        console.log("User connected", socket.id);
+        socket.on("disconnect", ()=>{
+            console.log("user disconnected", socket.id)
+        })
+    })
+
+    return io;
+};
+
+export const getIO = ()=>{
+    if(!io){
+        throw new Error("Socket.io not initialized");
+    }
+    return io;
+}
