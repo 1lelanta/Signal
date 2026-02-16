543d085f17024b8c9cffb7cab5d7fea3b7a9f825
1lelanta <kekikoki4@gmail.com>
2026-02-16 20:36:45 +0300

    moun error handler in app

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import corsMiddleware from "./config/cors.js";
-
+import { errorHandler } from "./middleware/error.middleware.js";
 
 const app = express();
 
@@ -10,5 +10,5 @@ app.use(corsMiddleware);
 app.get("/", (req, res)=>{
     res.json({message: "Signal API Running "})
 });
-
+app.use(errorHandler);
 export default app;
