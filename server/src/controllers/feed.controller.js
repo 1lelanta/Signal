e352f2f4a3aa8674390d0825219fdf1a75660574
1lelanta <kekikoki4@gmail.com>
2026-02-16 11:36:34 +0300

    create logic to feed user based on depthScore

---
+++ server/src/controllers/feed.controller.js
@@ -0,0 +1,14 @@
+import Post from "../models/Post.model.js";
+
+export const getFeed = async(req, res)=>{
+    try {
+        const posts = await Post.find({isPublished:true})
+        .sort({depthScore:-1})
+        .populate("author", "username reputationScore");
+
+        res.json(posts);
+
+    } catch (error) {
+        res.status(500).json({message:error.message})
+    }
+}
