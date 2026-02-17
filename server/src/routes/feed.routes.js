dd395f650c28c68193b5411b6ca06b7659bec547
1lelanta <kekikoki4@gmail.com>
2026-02-17 21:03:12 +0300

    add feed router

---
+++ server/src/routes/feed.routes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getFeed } from "../controllers/feed.controller.js";
+import { protect } from "../middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.get("/", protect, getFeed);
+export default router;
