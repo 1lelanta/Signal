414acae97fe5b6b39c23dc9bf385ee1088e531b6
1lelanta <kekikoki4@gmail.com>
2026-02-17 22:48:31 +0300

    mount auth routes  in app

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,19 @@ import express from "express";
 import corsMiddleware from "./config/cors.js";
 import { errorHandler } from "./middleware/error.middleware.js";
 
+import authRoutes from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import postRoutes from "./routes/post.routes.js";
+import commentRoutes from "./routes/comment.routes.js";
+import feedRoutes from "./routes/feed.routes.js";
+import reputationRoutes from "./routes/reputation.routes.js";
+
+import { errorHandler } from "./middleware/error.middleware.js";
+
+
+
+
+
 const app = express();
 
 app.use(express.json());
@@ -11,4 +24,8 @@ app.get("/", (req, res)=>{
     res.json({message: "Signal API Running "})
 });
 app.use(errorHandler);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users",);
+app.use("/api/posts")
 export default app;
