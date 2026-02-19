10e1919f98bfc312ab97470ed5f480184d9d4a1d
1lelanta <kekikoki4@gmail.com>
2026-02-19 19:30:21 +0300

    add notification emitter

--- server/src/sockets/notification.socket.js
+++ server/src/sockets/notification.socket.js
@@ -3,3 +3,10 @@ export const registerNotificationSocket = (io,socket, onlineUsers)=>{
         socket.emit("notificationReadSuccess")
     })
 }
+
+export const emitNotification = (io, onlineUsers,userId,data)=>{
+    const socketId = onlineUsers.get(userId.toString());
+    if(socketId){
+        io.to(socketId).emit("newNotification", data)
+    }
+}
