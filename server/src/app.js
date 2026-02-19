e2df863143afe20c9568aace2453992e834ffb55
1lelanta <kekikoki4@gmail.com>
2026-02-19 18:43:39 +0300

    mount each routes inside app

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,7 @@ import corsMiddleware from "./config/cors.js";
 import { errorHandler } from "./middleware/error.middleware.js";
 
 import authRoutes from "./routes/auth.routes.js";
-import userRouter from "./routes/user.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
 import commentRoutes from "./routes/comment.routes.js";
 import feedRoutes from "./routes/feed.routes.js";
@@ -23,9 +23,14 @@ app.use(corsMiddleware);
 app.get("/", (req, res)=>{
     res.json({message: "Signal API Running "})
 });
-app.use(errorHandler);
+
 
 app.use("/api/auth", authRoutes);
-app.use("/api/users",);
-app.use("/api/posts")
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes)
+app.use("/api/comments", commentRoutes);
+app.use("/api/feed", feedRoutes);
+app.use("/api/reputation", reputationRoutes)
+
+app.use(errorHandler);
 export default app;
