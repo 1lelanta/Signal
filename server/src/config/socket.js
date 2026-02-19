0c441670d9c4eceae373bc42cbda908996b39ae8
1lelanta <kekikoki4@gmail.com>
2026-02-19 19:34:50 +0300

    update socket configuration to handle registration of each sockets

--- server/src/config/socket.js
+++ server/src/config/socket.js
@@ -1,29 +1,45 @@
-import { Server} from "socket.io";
-import { ENV } from "./env.js";
+import { Server } from "socket.io";
+import { registerNotificationSocket } from "../sockets/notification.socket.js";
+import { registerCommentSocket } from "../sockets/comment.socket.js";
+import { registerReputationSocket } from "../sockets/reputation.socket.js";
 
 let io;
+const onlineUsers = new Map(); // userId -> socketId
 
-export const initSocket = (server)=>{
-    io = new  Server(server, {
-        cors:{
-            origin:ENV.CLIENT_URL,
-            methods:["GET", "POST"],
-        }
+export const initSocket = (server) => {
+  io = new Server(server, {
+    cors: {
+      origin: "*", // change in production
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("Connected:", socket.id);
+
+    socket.on("registerUser", (userId) => {
+      onlineUsers.set(userId, socket.id);
     });
 
-    io.on("connection", (socket)=>{
-        console.log("User connected", socket.id);
-        socket.on("disconnect", ()=>{
-            console.log("user disconnected", socket.id)
-        })
-    })
+    socket.on("disconnect", () => {
+      for (let [userId, socketId] of onlineUsers.entries()) {
+        if (socketId === socket.id) {
+          onlineUsers.delete(userId);
+          break;
+        }
+      }
+    });
 
-    return io;
+    // Register modular sockets
+    registerNotificationSocket(io, socket, onlineUsers);
+    registerCommentSocket(io, socket, onlineUsers);
+    registerReputationSocket(io, socket, onlineUsers);
+  });
 };
 
-export const getIO = ()=>{
-    if(!io){
-        throw new Error("Socket.io not initialized");
-    }
-    return io;
-}
+export const getIO = () => {
+  if (!io) throw new Error("Socket not initialized");
+  return io;
+};
+
+export const getOnlineUsers = () => onlineUsers;
