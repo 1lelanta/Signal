19d659c9f405f3a24497bf68ad4cfdb1f8263cfb
1lelanta <kekikoki4@gmail.com>
2026-02-20 13:06:12 +0300

    fix mongoose connection problem

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 import { ENV } from "./env.js";
 
-const connectDB = () => {
-  mongoose
-    .connect(ENV.MONGO_URI)
-    .then(() => console.log("MongoDB connected"))
-    .catch((error) => {
-      console.error("Database connection error:", error.message);
-      // Optional: remove process.exit(1) if you want server to keep running
-    });
+const connectDB = async () => {
+  try {
+    await mongoose.connect(ENV.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("Database connection error:", error.message);
+  }
 };
 
 export default connectDB;
