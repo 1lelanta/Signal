1bb6179a0915ce119b762869c3df91f29be3aa55
1lelanta <kekikoki4@gmail.com>
2026-02-21 10:35:18 +0300

    fix navbar view structure

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -1,27 +1,34 @@
 import { Link } from "react-router-dom";
 
-const Navbar = ()=>{
-    return(
-        <nav className="sticky top-0 z-50 bg-slate-900 border-b border-slate-800">
-            <div className="max-w-7-xl font-semibold tracking-tight items-center justify-between">
-                <Link to= "/" 
-                className="text-xl font-semibold tracking-tighy text-purple-400">
-                    SIGNAL
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 z-50 bg-slate-900 border-b border-slate-800">
+      <div className="max-w-7xl mx-auto px-4 flex items-center justify-between h-16">
+        
+        <Link 
+          to="/" 
+          className="text-xl font-semibold tracking-tight text-purple-400"
+        >
+          SIGNAL
+        </Link>
 
-                </Link>
-                
-                <div className="flex items-center gap-4">
-                    <Link to= "/profile"
-                        className="text-sm text-slate-400 hover:text-purple-400 transition">
-                    </Link>
-                        <button className="bg-purple-600 hover:bg-purple-700 px-4 py-1.5 rounded-md
-                        text-sm font-medium transition">
-                            New Post
-                        </button>
-                </div>
-            </div>
-        </nav>
-    )
-}
+        <div className="flex items-center gap-4">
+          
+          <Link 
+            to="/profile"
+            className="text-sm text-slate-400 hover:text-purple-400 transition"
+          >
+            Profile
+          </Link>
+
+          <button className="bg-purple-600 hover:bg-purple-700 px-4 py-1.5 rounded-md text-sm font-medium transition">
+            New Post
+          </button>
+
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
