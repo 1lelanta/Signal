6c38f90f01a5aa0fed628c97950357135fb005b8
1lelanta <kekikoki4@gmail.com>
2026-02-21 10:20:23 +0300

    add layout

---
+++ client/src/components/layout/layout.jsx
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+
+const layout = ({children})=>{
+    return(
+        <div className="min-h-screen bg-slate-950 text-slate-200">
+            <Navbar/>
+            <div className="flex">
+                {/* desktop Sidebar */}
+                <div className="hidden md:block">
+                    <Sidebar/>
+                </div>
+                    {/* Main Content */}
+                    <main className="flex-1 px-4 md:px-8 py-6 max-w-5xl mx-auto w-full">
+                        {children}
+                    </main>
+            </div>
+
+        </div>
+    )
+}
+
+export default layout
