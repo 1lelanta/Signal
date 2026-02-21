c4b9ccb565eeafa6e3204fbb1da8212698374457
1lelanta <kekikoki4@gmail.com>
2026-02-21 18:52:43 +0300

    add comment input component

--- client/src/components/comment/CommentInput.jsx
+++ client/src/components/comment/CommentInput.jsx
@@ -1,1 +1,26 @@
 import { useState } from "react";
+import Input from "../ui/input";
+import Button from "../ui/button";
+
+const CommentInput = ({onSubmit, parentId = null})=>{
+    const [text, setText] = useState("");
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        if(!text.trim()) return;
+
+        onSubmit(text, parentId);
+    };
+
+    return(
+        <form onSubmit={handleSubmit} className="flex gap-2 mt-2">
+            <Input 
+            value={text}
+            onChange={(e)=>setText(e.target.value)}
+            placeholder="Write a comment..."
+            />
+            <Button type="submit">Post</Button>
+
+        </form>
+    )
+}
