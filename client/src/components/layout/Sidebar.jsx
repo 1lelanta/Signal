6be4ea105548be606030c9f86141f0f9f47a6746
1lelanta <kekikoki4@gmail.com>
2026-02-21 10:13:20 +0300

    add sidebar components

---
+++ client/src/components/layout/Sidebar.jsx
@@ -0,0 +1,51 @@
+import { NavLink } from "react-router-dom";
+
+const linkBase = "block -x-4 py-2 rounded-md tex-sm transition";
+
+const Sidebar = ()=>{
+    return(
+        <aside className="w-64 h-[calc(100vh-56px)] border-r border-slate-800 bg-slate-900 p-4">
+            <nav className="space-y-2">
+                <NavLink 
+                to="/" className={({isActive})=>`
+                ${linkBase} ${
+                    isActive 
+                    ?"bg-purple-600 text-white"
+                    :"text-slate-400 hover:bg-slate-800"
+                }`}>
+                    Deep Feed
+                </NavLink>
+
+                <NavLink 
+                to="/explore" 
+                className= {({isActive})=> `${linkBase} ${
+                    isActive ? "bg-purple-600 text-white":"text-slate-400 hover:bg-slate-800"
+                }`}>
+                    Explore
+                </NavLink>
+
+                <NavLink to= "/notifications"
+                className={({isActive})=> 
+                `${linkBase}${
+                    isActive 
+                    ? "bg-purple-600 text-white": "text-slate-400 hover:bg-slate-800"
+                }`}>
+                    Notifications
+                </NavLink>
+
+                <NavLink to= "/reputation"
+                className={({isActive})=>`
+                ${linkBase} ${
+                    isActive ? "bg-purple-600 text-white"
+                    :"text-slate-400 hover:bg-slate-800"
+                }`}>
+                    Reputation
+                </NavLink>
+
+            </nav>
+
+        </aside>
+    )
+}
+
+export default Sidebar;
