f39f8cf7f3bf5d137d7841894912cada56b4fbac
1lelanta <kekikoki4@gmail.com>
2026-02-23 19:17:46 +0300

    feat(login): add login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useState } from "react";
+import { useAuth } from "../features/auth/useAuth";
+import Button from "../../components/ui/Button";
+import Input from "../../components/ui/Input";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const { login } = useAuth();
+  const [form, setForm] = useState({ email: "", password: "" });
 
-export default Login
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(form);
+  };
+
+  return (
+    <div className="max-w-md mx-auto bg-slate-900 p-6 rounded-xl mt-20">
+      <h2 className="text-xl font-bold mb-6 text-center">Login</h2>
+
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <Input
+          placeholder="Email"
+          onChange={(e) => setForm({ ...form, email: e.target.value })}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setForm({ ...form, password: e.target.value })}
+        />
+
+        <Button type="submit">Login</Button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
