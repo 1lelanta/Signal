efa9a099eb50df2f94617aaac6c7fb60ebd49f66
1lelanta <kekikoki4@gmail.com>
2026-02-23 19:14:47 +0300

    feat(home) add home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import PostComposer from "../components/post/PostComposer";
+import PostList from "../components/post/PostList";
+import { usePosts } from "../features/posts/usePosts";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const { posts, loading } = usePosts();
 
-export default Home
+  return (
+    <div className="max-w-2xl mx-auto space-y-6">
+
+      {/* Create Post */}
+      <PostComposer />
+
+      {/* Feed */}
+      {loading ? (
+        <div className="text-center py-10">Loading posts...</div>
+      ) : (
+        <PostList posts={posts} />
+      )}
+
+    </div>
+  );
+};
+
+export default Home;
