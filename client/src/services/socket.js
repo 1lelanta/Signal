3b2cfa862b877664383a80c68e35f4c4ab12b642
1lelanta <kekikoki4@gmail.com>
2026-02-23 21:14:49 +0300

    fix socket

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -2,7 +2,7 @@ import {io} from "socket.io-client"
 
 const socket = io("http://localhost:5000", {
     withCredentials: true,
-    authoConnect:false
+    autoConnect: false
 });
 
 export default socket;
