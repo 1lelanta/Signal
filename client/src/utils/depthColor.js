19c7f570b8a5c1a2491c089e1014827e1ac51f50
1lelanta <kekikoki4@gmail.com>
2026-02-23 19:22:58 +0300

    feat(depthColor): add depth color

--- client/src/utils/depthColor.js
+++ client/src/utils/depthColor.js
@@ -1,9 +1,6 @@
-import React from 'react'
-
-const depthColor = () => {
-  return (
-    <div>depthColor</div>
-  )
-}
-
-export default depthColor
+export const depthColor = (depth) => {
+  if (depth <= 2) return "bg-green-500/20 text-green-400";
+  if (depth <= 5) return "bg-blue-500/20 text-blue-400";
+  if (depth <= 10) return "bg-purple-500/20 text-purple-400";
+  return "bg-red-500/20 text-red-400";
+};
