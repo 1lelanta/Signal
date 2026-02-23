cb2df069b2f7f719bf35ee146b191c045af92585
1lelanta <kekikoki4@gmail.com>
2026-02-23 20:48:36 +0300

    fix(postUse):

--- client/src/features/posts/usePosts.js
+++ client/src/features/posts/usePosts.js
@@ -1,4 +1,4 @@
-(import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
 import api from "../../services/axios";
 
 export function usePosts(userId = null) {
