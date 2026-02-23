41464a11d530f91da5c3d18bca719c9ed6335183
1lelanta <kekikoki4@gmail.com>
2026-02-23 19:00:07 +0300

    feat(Input): add input

--- client/src/components/ui/Input.jsx
+++ client/src/components/ui/Input.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-
-const input = () => {
+const Input = ({
+  className = "",
+  ...props
+}) => {
   return (
-    <div>input</div>
-  )
-}
+    <input
+      className={`w-full bg-slate-800 border border-slate-700 rounded-md px-3 py-2 text-sm text-white focus:outline-none focus:ring-2 focus:ring-purple-500 ${className}`}
+      {...props}
+    />
+  );
+};
 
-export default input
+export default Input;
