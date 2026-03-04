dc4ff7c2dfff27ec42e26c969e29ffda3ced92a0
1lelanta <kekikoki4@gmail.com>
2026-03-04 18:56:15 +0300

    feat(cors): enhance CORS configuration to allow multiple origins

--- server/src/config/cors.js
+++ server/src/config/cors.js
@@ -2,8 +2,30 @@ import cors from 'cors'
 
 import { ENV } from './env.js'
 
+const allowedOrigins = [
+  ENV.CLIENT_URL,
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "http://localhost:5174",
+  "http://127.0.0.1:5174",
+].filter(Boolean);
+
 const corsOptions = {
-    origin:ENV.CLIENT_URL,
+    origin(origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        const isAllowed =
+            allowedOrigins.includes(origin) ||
+            /^https?:\/\/(localhost|127\.0\.0\.1):(5173|5174|5175)$/.test(origin);
+
+        if (isAllowed) {
+            return callback(null, true);
+        }
+
+        return callback(new Error("Not allowed by CORS"));
+    },
     credentials:true
 };
 
