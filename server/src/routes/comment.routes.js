ce4649f7bdcd44b8b0798ca26c5bac42a5bf4c91
1lelanta <kekikoki4@gmail.com>
2026-03-04 19:21:08 +0300

    feat(CommentRoutes): add GET endpoint to retrieve comments by post ID

--- server/src/routes/comment.routes.js
+++ server/src/routes/comment.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addComment } from "../controllers/comment.controller.js";
+import { addComment, getCommentsByPost } from "../controllers/comment.controller.js";
 import { protect } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+router.get("/post/:postId", getCommentsByPost);
 router.post("/:postId", protect, addComment);
 
 export default router;
