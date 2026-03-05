c6835188f1382e8457b12af2b1abf0e5cc62a20d
1lelanta <kekikoki4@gmail.com>
2026-03-05 19:06:48 +0300

    feat(Providers): wrap AuthProvider with ThemeProvider for theme management

--- client/src/app/providers.jsx
+++ client/src/app/providers.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { AuthProvider } from "../features/auth/authContext";
+import { ThemeProvider } from "./themeContext";
 
 const Providers = ({ children }) => {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <ThemeProvider>
+      <AuthProvider>{children}</AuthProvider>
+    </ThemeProvider>
+  );
 };
 
 export default Providers;
